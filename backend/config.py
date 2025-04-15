46b495a427250b95da6fec2740eee7b5ec37667d
keerthibollaju <keerthibollaju@example.com>
2025-04-15 19:30:16 +0530

    Add Procfile and update backend files for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
 NEWSAPI_KEY = os.getenv("NEWSAPI_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY") 
